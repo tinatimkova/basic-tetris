cbe78fae4dc25d557847a9570587baa7c3419992
timosha <tinaatimkovaa@gmail.com>
2020-06-03 13:30:03 -0400

    Build a tetris with basic functionality
    Need to move the last row to the beginning of the grid, so that we
    can have the grid of the same size;
    Need to write a gameOver() function;

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
   let squares = Array.from(document.querySelectorAll('.grid div'))
-  const ScoreDisplay = document.querySelector("#score")
-  const StartBtn = document.querySelector('#start-button')
+  const scoreDisplay = document.querySelector("#score")
+  const startBtn = document.querySelector('#start-button')
   const width = 10
+  let nextRandom = 0
+  let timerId
+  let score = 0
 
   // The Tetrominoes
   const lTetromino = [
@@ -65,7 +68,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 //make the tetromino move down every second
-timerId = setInterval(moveDown, 1000)
+// timerId = setInterval(moveDown, 1000)
 
 //assign functions to keyCodes
 function control(e) {
@@ -94,10 +97,13 @@ function freeze() {
   if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
     current.forEach(index => squares[currentPosition + index].classList.add('taken'))
     //start a new tetromino falling
-    random = Math.floor(Math.random()*theTetrominoes.length)
+    random = nextRandom
+    nextRandom = Math.floor(Math.random()*theTetrominoes.length)
     current = theTetrominoes[random][currentRotation]
     currentPosition = 4
     draw()
+    displayShape()
+    addScore()
   }
 }
 
@@ -144,4 +150,56 @@ function rotate() {
 const displaySquares = document.querySelectorAll('.mini-grid div')
 const displayWidth = 4
 let displayIndex = 0
+
+//the Tetrominos without rotations
+const upNextTetrominoes = [
+  [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
+  [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino
+  [1, displayWidth, displayWidth+1, displayWidth+2], //tTetromino
+  [0, 1, displayWidth, displayWidth+1], //oTetromino
+  [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] //iTetromino
+]
+
+//display the shape in the mini-grid display
+function displayShape() {
+  //remove any trace of a tetromino from the entire grid
+  displaySquares.forEach(square => {
+    square.classList.remove('tetromino')
+  })
+  upNextTetrominoes[nextRandom].forEach(index => {
+    displaySquares[displayIndex + index].classList.add('tetromino')
+  })
+}
+
+//add functionality to the button
+startBtn.addEventListener('click', () => {
+if (timerId) {
+  clearInterval(timerId)
+  timerId = null
+} else {
+  draw()
+  timerId = setInterval(moveDown, 1000)
+  nextRandom = Math.floor(Math.random()*theTetrominoes.length)
+  displayShape()
+}
+})
+
+// add score
+function addScore() {
+  for (let i=0; i < 199; i+= width) {
+    const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+
+    if (row.every(index => squares[index].classList.contains('taken'))) {
+      score += 10
+      scoreDisplay.innerHTML = score
+      row.forEach(index => {
+        squares[index].classList.remove('taken')
+        squares[index].classList.remove('tetromino')
+      })
+      const squaresRemoved = squares.splice(i, width)
+      squares = squaresRemoved.concat(squares)
+      // squares.forEach(cell => grid.appendChild(cell))
+    }
+  }
+}
 })
